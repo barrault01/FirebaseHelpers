72206d9ff81719656723a44ce5694b78d67d112c
Antoine Barrault <barrault01@gmail.com>
2021-02-06 11:19:12 -0300

    Update swiftcrypto

--- Package.swift
+++ Package.swift
@@ -12,8 +12,10 @@ let package = Package(
             targets: ["FirebaseHelpers"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/barrault01/google-auth-library-swift.git", from: "0.5.1")
+        .package(url: "https://github.com/barrault01/google-auth-library-swift.git", from: "0.5.1"),
+            .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0")
     ],
+    
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
